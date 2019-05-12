e8bf2aa0fa6011bc5490ac6bdb4ec3b7b0d8f645
Guy Nesher <nesher.guy@gmail.com>
2019-05-12 14:13:46 +0300

    upgrade capacitor / phaser

--- src/game.ts
+++ src/game.ts
@@ -3,7 +3,7 @@ import { MainScene } from "./scenes/main-scene";
 declare const BUILD_TARGET: string;
 
 // main game configuration
-const config: GameConfig = {
+const config: Phaser.Types.Core.GameConfig = {
     width: 800,
     height: 600,
     type: Phaser.AUTO,
@@ -15,7 +15,7 @@ const config: GameConfig = {
 };
 
 export class Game extends Phaser.Game {
-    constructor(config: GameConfig) {
+    constructor(config: Phaser.Types.Core.GameConfig) {
         super(config);
     }
 }
